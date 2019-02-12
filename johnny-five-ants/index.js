4cd929a0281039dd45f687b3b1bb44f5875044b8
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2019-02-12 22:25:11 +0100

    Use Yarn instead of NPM for Johnny-Five and Raspi-IO.

--- johnny-five-ants/index.js
+++ johnny-five-ants/index.js
@@ -6,24 +6,22 @@ const board = new five.Board({
 });
 
 // LEDS = PIN (5,12,16,6,13,17)
-const leds = [new five.Led(5), new five.Led(12), new five.Led(16), new five.Led(6), new five.Led(13), new five.Led(17)]
 
 board.on("ready", function() {
-    leds[0].blink();
-    leds[1].blink();
-    leds[2].blink();
-    leds[3].blink();
-    leds[4].blink();
-    leds[5].blink();
+    const leds = [
+        new five.Led('GPIO5'), new five.Led('GPIO12'), new five.Led('GPIO16'),
+	new five.Led('GPIO6') ,new five.Led('GPIO13'), new five.Led('GPIO17')
+    ]
+
+    leds.forEach(led => {
+        led.blink();
+    });
 
     // When this script is stopped, turn the LED off
     // This is just for convenience
     this.on('exit', function() {
-        leds[0].stop().off();
-        leds[1].stop().off();
-        leds[2].stop().off();
-        leds[3].stop().off();
-        leds[4].stop().off();
-        leds[5].stop().off();
+        leds.forEach(led => {
+            led.stop().off();
+        });
     });
 });
